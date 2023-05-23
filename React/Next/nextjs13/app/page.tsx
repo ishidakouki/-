cd936a7ecb13bb38c0e82db1e2e6849fa1d22a7a
ishidakouki <kouki9230badahari@outlook.jp>
2023-05-24 01:34:02 +0900

    nextjs作成

---
+++ React/Next/nextjs13/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page () {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Link href="/page1">ページ1に遷移</Link>
+    </>
+  )
+}
---
+++ React/Next/nextjs13/app/page1/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page () {
+  return (
+    <>
+      <h1>ページ2</h1>
+      <Link href="/page2">ページ2に遷移</Link>
+    </>
+  )
+}
