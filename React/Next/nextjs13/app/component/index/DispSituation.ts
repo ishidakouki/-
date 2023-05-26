32dcdac638a9060f1c32b8f4a912ccc33c6a81c1
石田高基 <k-ishida@mcast.co.jp>
2023-05-27 00:13:37 +0900

    型を指定

--- React/Next/nextjs13/app/component/index/DispSituation.ts
+++ React/Next/nextjs13/app/component/index/DispSituation.ts
@@ -1,4 +1,9 @@
-export const DispSituation = Object.freeze({
+type DispSituation = {
+    [key: number]: string;
+    default: string;
+};
+
+export const DispSituation:DispSituation = Object.freeze({
     0: "放送中",
     10: "放送予定",
     20: "作成中",
